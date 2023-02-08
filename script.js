80710cb47787398cee7b19a9caa3d32414503a63
Nazar <ns15052004@gmail.com>
2023-02-08 19:37:28 +0200

    set up the formatting

--- script.js
+++ script.js
@@ -5,27 +5,22 @@ async function getCurrence(url) {
         const currencies = await fetch(url);
         const result = await currencies.json();
 
-        result.sort((a,b)=>{
-            if(a.cc<b.cc)
-            {
+        result.sort((a, b) => {
+            if (a.cc < b.cc) {
                 return -1;
             }
 
-            if(a.cc>b.cc)
-            {
+            if (a.cc > b.cc) {
                 return 1;
             }
 
-            if(a.cc === b.cc)
-            {
+            if (a.cc === b.cc) {
                 return 0;
             }
         })
 
-        for(let i=0;i<result.length;i++)
-        {
-            for(let j=0;j<selects.length;j++)
-            {
+        for (let i = 0; i < result.length; i++) {
+            for (let j = 0; j < selects.length; j++) {
                 let option = document.createElement("option");
                 option.value = result[i].rate;
                 option.textContent = result[i].cc;
@@ -33,20 +28,18 @@ async function getCurrence(url) {
                 selects[j].append(option);
             }
         }
-    }
-    catch (err){
+    } catch (err) {
         const modalWindow = document.querySelector(".modalWindow");
         modalWindow.classList.add("modalWindow-open");
-        modalWindow.querySelector("h1").addEventListener("selectstart",function (event){
+        modalWindow.querySelector("h1").addEventListener("selectstart", function (event) {
             event.preventDefault();
         })
         console.error(err);
     }
 }
 
-function swap(event){
-    if(selects[0].value === selects[1].value)
-    {
+function swap(event) {
+    if (selects[0].value === selects[1].value) {
         return;
     }
 
@@ -62,12 +55,9 @@ function swap(event){
 }
 
 function selected() {
-    if(selects[0].value !== selects[1].value)
-    {
-        if(inputs[0].disabled === true && selects[0].value !== "-1" && selects[1].value !== "-1")
-        {
-            for(let i=0;i<inputs.length;i++)
-            {
+    if (selects[0].value !== selects[1].value) {
+        if (inputs[0].disabled === true && selects[0].value !== "-1" && selects[1].value !== "-1") {
+            for (let i = 0; i < inputs.length; i++) {
                 inputs[i].disabled = false;
             }
         }
@@ -75,30 +65,25 @@ function selected() {
         return;
     }
 
-    for(let i=0;i<inputs.length;i++)
-    {
+    for (let i = 0; i < inputs.length; i++) {
         inputs[i].disabled = true;
     }
 }
 
-function inputSelection(event){
-    if(this.classList.contains("first-input"))
-    {
-        conversion.call(this,inputs[1],selects[0][selects[0].selectedIndex].value,selects[1][selects[1].selectedIndex].value);
-    }
-    else
-    {
-        conversion.call(this,inputs[0],selects[0][selects[0].selectedIndex].value,selects[1][selects[1].selectedIndex].value);
+function inputSelection(event) {
+    if (this.classList.contains("first-input")) {
+        conversion.call(this, inputs[1], selects[0][selects[0].selectedIndex].value, selects[1][selects[1].selectedIndex].value);
+    } else {
+        conversion.call(this, inputs[0], selects[0][selects[0].selectedIndex].value, selects[1][selects[1].selectedIndex].value);
     }
 }
 
 function conversion(input, leftCurrency, rightCurrency) {
-    input.value = Math.round(((this.value * leftCurrency)/rightCurrency)*100)/100;
+    input.value = Math.round(((this.value * leftCurrency) / rightCurrency) * 100) / 100;
 }
 
 function minusAndPlusDisabledInput(event) {
-    if(event.code === "Equal" || event.code === "Minus" || event.code === "Comma")
-    {
+    if (event.code === "Equal" || event.code === "Minus" || event.code === "Comma") {
         event.preventDefault();
         return;
     }
@@ -112,15 +97,13 @@ let arrow = document.querySelector(".arrow");
 
 getCurrence(url);
 
-for(let i=0;i<selects.length;i++)
-{
-    selects[i].addEventListener("change",selected)
+for (let i = 0; i < selects.length; i++) {
+    selects[i].addEventListener("change", selected)
 }
 
-for(let i=0;i<inputs.length;i++)
-{
-    inputs[i].addEventListener("input",inputSelection)
-    inputs[i].addEventListener("keydown",minusAndPlusDisabledInput)
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", inputSelection)
+    inputs[i].addEventListener("keydown", minusAndPlusDisabledInput)
 }
 
-arrow.addEventListener("click",swap);
+arrow.addEventListener("click", swap);
